dbcc187bf8f4759f959fa322075c2f171629e3da
Mohit Vaswani <mohitvaswani010@gmail.com>
2022-11-24 14:29:51 +0530

    Home page completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,36 @@
 import Navbar from "../Components/Navbar";
 import Background from "../Components/Background";
+import { useRef } from "react";
 
 const Home = () => {
-    return(
-        <>
-        <Background/>
-        <Navbar/>
-        </>
-    )
-}
+  const welcome = useRef(null);
+  const space = useRef(null);
+  const paragraph = useRef(null);
 
-export default Home
+  return (
+    <main>
+      <Background />
+      <Navbar />
+      <section>
+        <div>
+          <div className="mainText">
+            <h3 ref={welcome}>Welcome to the</h3>
+            <h1 ref={space}>Space</h1>
+          </div>
+          <p ref={welcome}>
+            Welcome to the fascinating and entertaining world of space. Here,
+            you can play a little quiz game while learning about several
+            planets.
+          </p>
+          <a href="/">
+          <button className="btn wobble-hor-bottom">
+            Explore
+          </button>
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
